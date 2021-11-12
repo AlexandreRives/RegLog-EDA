5d0e18cddf918e7017b84442292442da032244fb
Deffa <deffandiaye3@gmail.com>
2021-11-12 08:58:34 +0100

    Modification of formula_function

--- R/formula_function.R
+++ R/formula_function.R
@@ -7,35 +7,10 @@
 #' 
 #' @export
 #' 
-f_Formula <- function(df) {
-  ANSWER <- readline("")
-  y<-sub("\\~.*", "", ANSWER)
-  x<-sub('.*~', '', ANSWER)
-  var_explicatives=strsplit(x,split="[+]")
+
+f_Formula <- function(formula,donnee) {
   
-  if(length(as.list(var_explicatives[[1]]))>1){
-    
-    
-    #model.frame(df[[y]]~ paste(df[[unlist(var_explicatives)]], collapse= "+"))
-    
-    formule <- as.formula(paste(str_c(y," ~ "), paste(unlist(var_explicatives), collapse= "+")))
-    model.frame(formule,data=df)
-    
-    
-  }
-  else if(length(as.list(var_explicatives[[1]]))==1){
-    
-    
-    #model.frame(df[[y]]~df[[x]])
-    formule <- as.formula(str_c(y," ~ ",x))
-    model.frame(formule,data=df)
-  }
+  df<- model.frame(formula,data=donnee)
   
-  else if(x=="."){
-    
-    formule <- as.formula(paste(str_c(y," ~ "), paste(unlist(colnames(df)), collapse= "+")))
-    model.frame(formule,data=df)
-    
-  }
-  
+  return(df)
 }
