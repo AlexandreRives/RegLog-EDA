1520f12550444b17b11ed4b0491ddad2327a4dd4
Deffa <deffandiaye3@gmail.com>
2021-11-19 08:05:37 +0100

    modification on fit_function

--- R/fit_function.R
+++ R/fit_function.R
@@ -2,65 +2,87 @@
 #'
 #' Function that return a trained dataset using the gradient descent.
 #' 
+#' 
 #' @param formula the data frame
 #' @param data the data frame
 #' @param ncores the number of cores to run the fit function faster. It's better to use it with more than 10 000 data.
 #' @param batch_size the number of observations for the mini batch mode.
 #' @param normalize TRUE if you want to scale your features. It doesn't take the qualitative data.
+#' @param learning_rate the learning_rate for the logistic regression
+#' @param nb-iteration the number of iterations for the gradient descent 
 #' 
-#' @author Alexandre Rives
+#' 
+#' @author Deffa Ndiaye
 #' 
 #' @export
 #' 
 #' @return A fitted dataset
-#' 
-fit <- function(formula, data, ncores, batch_size, norm){
-  if(data == NULL){
-    print("You should enter a data set.")
+#' @source 
+
+
+
+fit_reg_log <- function(formula,data, mode, batch_size, normalize = FALSE,learning_rate,nb_iteration){
+  
+  #data.frame control
+  ok <- is.data.frame(data)
+  if (!ok){
+    stop("This is not a data.frame")
   }
-  else{
-    vector_x_y <- f_Formula(data)
-    df_x <- filtered_x(data[vector_x_y[1]])
-    df_y <- filtered_y(data[vector_x_y[2]])
-    if(norm == TRUE){
-      df_x_normalize <- normalize(df_x)
-      df_x_dummies <- dummies(df_x)
-      df_x_final <- cbind(df_x_dummies, df_x_normalize)
-      df_y_dummies <- dummies(df_y)
-    }else{
-      df_x_dummies <- dummies(df_x)
-      split_df_x <- splitmix(df_x)
-      df_x_quanti <- split_df_x$X.quanti
-      df_x_final <- cbind(df_x_dummies, df_x_quanti)
-      df_y_dummies <- dummies(df_y)
-    }
+  
+  
+  df <- f_Formula(data)
+  y <- df[1]
+  
+  #normalization
+  if(norm == TRUE){
+    X <- normalize(X)
+    
+  }else{
+    X <- df[,-1]
+    
   }
+  #gradient descent
+  
+  if (mode == "batch"){
+    coefs <- descente_de_gradient_ok(df,colnames(X),colnames(Y),learning_rate,nb_iteration)
+  } else if (mode == "online"){
+    coefs <- descente_de_gradient_online(df,colnames(X),colnames(Y),learning_rate,nb_iteration)
+  } else if (mode == "mini_batch"){
+    coefs <- descente_de_gradient_mini_batch(df,colnames(X),colnames(Y),batch_size,learning_rate,nb_iteration)
+  }
+  
+
+  #return(coefs)
+
+  #instanciation
+  instance <- list()
+  instance$df <- data
+  instance$coefficients <-coefs
+  class(instance) <- "reg_log"
+  return(instance)
+  
 }
 
 
-# En fait on ne prend en entre que des variables quanti donc pas besoin de tout refaire dans le fit (vu que ca sera fait en amont)
-fit_reg_log <- function(formula, data, mode, batch_size, ncores, normalize = FALSE, autres param. éventuels){
-  if (data == NULL) {print("You should enter a data set.")}
+#Metrics 
+
+#Null Deviance
+
+#Residual Deviance 
+
+
+#AIC
+
+
+#surcharge de print
+print.fit_reg_log <- function(object){
+  #affichage de la liste des variables
+  cat("Variables : ", colnames(object$df),"\n")
   
-  # # Etape 1 : Récuperer les valeurs de formula
-  # var_x = fct_formula(formula)$x
-  # var_y = fct_formula(formula)$y
-  # 
-  # # Etape 2 : Filter le dataframe
-  # df_x = filter_df(data, var_x)
-  # df_y = filter_df(data, var_y)
-  
-  # Etape 3 : Si normalize = TRUE, on normalise les données sinon rien 
-  if (normalize == TRUE) {df_x = get_normalize_df(df_x)}
-  
-  # Etape 4 : Descente du gradient en fonction du mode
-  if (mode == "batch"){
-    coefs = descente_de_gradient_batch(.............)
-  } else if (mode == "online"){
-    coefs = descente_de_gradient_online(.............)
-  } else if (mode == "mini_batch"){
-    coefs = descente_de_gradient_mini_batch(.............)
-  }
+}
+
+
+fit_reg_log(species~sepal_length+sepal_width+petal_length+petal_width,data=iris,mode="batch",normalize = TRUE,learning_rate =0.01 ,nb_iteration = 2)
   
   # Etape 5 : Sortir toutes les metriques neccessaires 
   
